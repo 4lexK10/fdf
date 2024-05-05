78bfcd9ca2bf4d41be45ff9ed49aab9aa5b6856b
Alexander Klostermann <akloster@fu-r3-p8.s19.be>
2024-05-05 20:30:58 +0200

    lil addadge

--- create_3d.c
+++ create_3d.c
@@ -6,7 +6,7 @@
 /*   By: akloster <akloster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 19:03:22 by akloster          #+#    #+#             */
-/*   Updated: 2024/05/01 19:28:29 by akloster         ###   ########.fr       */
+/*   Updated: 2024/05/05 20:28:48 by akloster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,25 @@ static char	*simplify(char **raw_line);
 int	*create_3d(fd)
 {
 	char	*raw;
-	char	**line;
+	char	**raw_line;
 	char	*map_3d;
-	
+	int		size;
 
+	size = 0;
 	raw = clean_line(get_next_line(fd));
-	line = ft_split(raw, ' ');
-	map_3d = simplify(line);
+	raw_line = ft_split(raw, ' ');
+	while (raw_line[size] != NULL)
+		++size;
+	map_3d = simplify(raw_line, size);
 }
 
-static char	*simplify(char **raw_line)
+static char	*simplify(char **raw_line, int size)
 {
-	char	*simp;
 	char	*buf;
 	int		i;
+	int		*line;
 
-	simp = ft_strdup(raw_line[0])
+	line = (int)malloc(sizeof(int) * size);
 	while (raw_line[i] != NULL)
 	{
 		
